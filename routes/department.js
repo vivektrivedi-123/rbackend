b8915e985baa07873d2a7d6ac3f7c002859ae31c
Apurva <apurva@rudrainnovative.in>
2021-04-08 19:47:55 +0530

    changes In department.js

--- routes/department.js
+++ routes/department.js
@@ -3,4 +3,4 @@ const express = require("express");
 const _ = require("lodash");
 const router = express.Router();
 
-router.get("/api/v1/getdept" , deptcontroller.)
+router.get("/api/v1/getdept", deptcontroller.getdept);
