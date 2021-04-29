237b03e347d9cd0cde4bc8f85a48033db2b15d27
apurva571 <apurva@rudrainnovative.in>
2021-04-29 19:42:38 +0530

    validation

--- validation/fieldValidation.js
+++ validation/fieldValidation.js
@@ -3,7 +3,7 @@ const { body, validationResult } = require("express-validator");
 const fieldValidation = () => {
   return [
     body("field_name").isLength({ min: 3, max: 50 }),
-    body("field_type").isLength({ min: 5, max: 50 }),
+    body("field_type").isLength({ min: 10, max: 50 }),
     body("field_options").isLength({ min: 5, max: 50 }),
   ];
 };
