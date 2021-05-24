50e9a22f4a99336b699b663a2711da834f49d205
apurva571 <apurva@rudrainnovative.in>
2021-05-24 18:43:40 +0530

    location updated

--- validation/locationValidation.js
+++ validation/locationValidation.js
@@ -8,7 +8,7 @@ const locationValidation = () => {
     body("location_state").isLength({ min: 3, max: 20 }),
     body("postal_code").isPostalCode("IN"),
     body("country").isLength({ min: 2, max: 20 }),
-  ];
+ ];
 };
 const validateSchema = (req, res, next) => {
   const errors = validationResult(req);
